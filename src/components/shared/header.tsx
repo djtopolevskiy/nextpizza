8dc0fd637b7c31f05375f384b205e2a4c6bdbcc2
djtopolevskiy <djtopolevskiy@gmail.com>
2024-10-04 21:26:32 +0300

    202410042126

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -7,6 +7,9 @@ import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { Container } from './container'
 import { SearchInput } from './search-input'
+import { CartDrawer } from './cart-drawer'
+import { Button } from '../ui/button'
+import { ArrowRight, ShoppingCart } from 'lucide-react'
 // import { AuthModal } from './modals/auth-modal';
 // import { ProfileButton } from './profile-button';
 // import toast from 'react-hot-toast';
@@ -66,6 +69,22 @@ export const Header: React.FC<Props> = ({
 
 				<div className='mx-10 flex-1'>
 					<SearchInput />
+				</div>
+
+				<div className='flex items-center gap-3'>
+					<Button variant='outline'>Войти</Button>
+
+					<CartDrawer>
+						<Button className='group relative'>
+							<b>520 ₽</b>
+							<span className='h-full w-[1px] bg-white/30 mx-3' />
+							<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+								<ShoppingCart className='h-4 w-4 relative' strokeWidth={2} />
+								<b>3</b>
+							</div>
+							<ArrowRight className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0' />
+						</Button>
+					</CartDrawer>
 				</div>
 
 				{/* {hasSearch && (

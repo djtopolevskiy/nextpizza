42658364d934df48fa3010b60f3aa556706a9f0a
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-11-15 14:56:15 +0200

    202415111456

--- src/shared/components/shared/modals/choose-product-modal.tsx
+++ src/shared/components/shared/modals/choose-product-modal.tsx
@@ -1,21 +1,18 @@
-'use client'
+"use client"
 
 import {
 	Dialog,
 	DialogContent,
 	DialogDescription,
 	DialogTitle,
-} from '@/shared/components/ui/dialog'
-import { cn } from '@/shared/lib/utils'
-import { useCartStore } from '@/shared/store'
-import { Ingredient, ProductItem } from '@prisma/client'
-import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-import toast from 'react-hot-toast'
-import { ProductWithRelations } from '../../../../../@types/prisma'
-import { ChoosePizzaForm } from '../choose-pizza-form'
-import { ChooseProductForm } from '../choose-product-form'
+} from "@/shared/components/ui/dialog"
+import { cn } from "@/shared/lib/utils"
+import { Ingredient, ProductItem } from "@prisma/client"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
+import { useRouter } from "next/navigation"
+import React from "react"
+import { ProductWithRelations } from "../../../../../@types/prisma"
+import { ProductForm } from "../product-form"
 
 interface Props {
 	product: ProductWithRelations
@@ -25,42 +22,14 @@ interface Props {
 	onOpenChange?: () => void
 }
 
-export const ChooseProductModal: React.FC<Props> = ({
-	productItem,
-	product,
-	ingredients,
-	className,
-}) => {
+export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
 	const router = useRouter()
-	const firstItem = product.items[0]
-	const isPizzaForm = Boolean(firstItem.pizzaType)
-	const [addCartItem, loading] = useCartStore(state => [
-		state.addCartItem,
-		state.loading,
-	])
-
-	const onSubmit = async (productItemId?: number, ingredients?: number[]) => {
-		try {
-			const itemId = productItemId ?? firstItem.id
-
-			await addCartItem({
-				productItemId: itemId,
-				ingredients,
-			})
-			console.log(ingredients, 'ingredients', 'productItemId', itemId)
-			toast.success(product.name + ' добавлена в корзину')
-			router.back()
-		} catch (error) {
-			toast.error('Не удалось добавить товар в корзину')
-			console.error(error)
-		}
-	}
 
 	return (
 		<Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
 			<DialogContent
 				className={cn(
-					'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden',
+					"p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden",
 					className
 				)}
 			>
@@ -68,25 +37,7 @@ export const ChooseProductModal: React.FC<Props> = ({
 					<DialogTitle>HiddenTitle</DialogTitle>
 					<DialogDescription>HiddenDescription</DialogDescription>
 				</VisuallyHidden.Root>
-				{isPizzaForm ? (
-					<ChoosePizzaForm
-						imageUrl={product.imageUrl}
-						name={product.name}
-						price={productItem.price}
-						ingredientsinform={ingredients}
-						items={product.items}
-						onSubmit={onSubmit}
-						loading={loading}
-					/>
-				) : (
-					<ChooseProductForm
-						imageUrl={product.imageUrl}
-						name={product.name}
-						price={firstItem.price}
-						onSubmit={onSubmit}
-						loading={loading}
-					/>
-				)}
+				<ProductForm product={product} onSubmit={() => router.back()} />
 			</DialogContent>
 		</Dialog>
 	)
--- src/shared/components/shared/product-form.tsx
+++ src/shared/components/shared/product-form.tsx
@@ -1,15 +1,16 @@
-'use client'
+"use client"
 
-import { useCartStore } from '@/shared/store'
-import React from 'react'
-import toast from 'react-hot-toast'
-import { ProductWithRelations } from '../../../../@types/prisma'
-import { ChoosePizzaForm } from './choose-pizza-form'
-import { ChooseProductForm } from './choose-product-form'
+import { useCartStore } from "@/shared/store"
+import React from "react"
+import toast from "react-hot-toast"
+import { ProductWithRelations } from "../../../../@types/prisma"
+import { ChoosePizzaForm } from "./choose-pizza-form"
+import { ChooseProductForm } from "./choose-product-form"
 
 interface Props {
 	product: ProductWithRelations
 	onSubmit?: VoidFunction
+	className?: string
 }
 
 export const ProductForm: React.FC<Props> = ({
@@ -33,11 +34,11 @@ export const ProductForm: React.FC<Props> = ({
 				ingredients,
 			})
 
-			toast.success(product.name + ' добавлена в корзину')
+			toast.success(product.name + " добавлена в корзину")
 
 			_onSubmit?.()
 		} catch (err) {
-			toast.error('Не удалось добавить товар в корзину')
+			toast.error("Не удалось добавить товар в корзину")
 			console.error(err)
 		}
 	}

65cf0b73fbbb294b1675d6fada6af0cde505e366
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-11-21 14:14:44 +0200

    202411211414

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,8 +1,8 @@
 // export { calcTotalPizzaPrice } from './calc-total-pizza-price';
-export { getAvailablePizzaSizes } from './get-available-pizza-sizes'
-export { getCartDetails } from './get-cart-details'
-export { getCartItemDetails } from './get-cart-item-details'
-export { getPizzaDetails } from './get-pizza-details'
+export { getAvailablePizzaSizes } from "./get-available-pizza-sizes"
+export { getCartDetails } from "./get-cart-details"
+export { getCartItemDetails } from "./get-cart-item-details"
+export { getPizzaDetails } from "./get-pizza-details"
 // export { calcCartItemTotalPrice } from './calc-cart-item-total-price';
-// export { sendEmail } from './send-email';
-// export { createPayment } from './creat-payment';
+export { createPayment } from "./creat-payment"
+export { sendEmail } from "./send-email"
